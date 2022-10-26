727465f949fcb53ad89dc2ab9b752c394e3d56b3
igor531205 <41549374+igor531205@users.noreply.github.com>
2022-10-26 22:05:13 +0300

    job...

--- bot.py
+++ bot.py
@@ -1,3 +1,41 @@
+def botik():
+    """Start the bot."""
+    from telegram import ForceReply, Update
+    from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /start is issued."""
+        user = update.effective_user
+        await update.message.reply_html(
+            rf"Hi {user.mention_html()}!",
+            # reply_markup=ForceReply(selective=True),
+        )
+
+    async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Send a message when the command /help is issued."""
+        await update.message.reply_text("Help!")
+
+    async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Echo the user message."""
+        await update.message.reply_text(update.message.text)
+
+    TOKEN = '5484436077:AAHRLCdLdDzLyRbU2M3iWsRkOHIM_DmFQeE'
+
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
 def bot():
     """telegram bot.
     """
@@ -203,5 +241,5 @@ if __name__ == '__main__':
 
     from os import system
 
-    system('cls')
-    bot()
+    # system('cls')
+    botik()
